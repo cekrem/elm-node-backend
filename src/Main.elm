7282f3f419d3f55dd206866aa85fa8763bdfb1e3
Christian Ekrem <christianekrem@gmail.com>
2025-12-18 15:36:51 +0100

    make things less stringly typed (we're in elm!)

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,12 @@
 port module Main exposing (main)
 
-import Dict exposing (Dict)
 import Json.Decode as Decode exposing (Decoder)
 import Json.Encode as Encode
 import Platform
 
 
 
--- PORTS (request in, response out via callback)
+-- PORTS
 
 
 port request : (Decode.Value -> msg) -> Sub msg
@@ -20,22 +19,46 @@ port response : Encode.Value -> Cmd msg
 -- TYPES
 
 
+type RequestId
+    = RequestId String
+
+
+type Method
+    = Get
+    | Head
+    | Post
+    | Put
+    | Delete
+    | Connect
+    | Options
+    | Trace
+    | Patch
+
+
+type Status
+    = Ok
+    | BadRequest
+    | NotFound
+    | InternalError
+
+
 type alias Request =
-    { id : String
-    , method : String
+    { id : RequestId
+    , method : Method
     , path : String
     , body : String
-    , headers : Dict String String
     }
 
 
 type alias Response =
-    { id : String
-    , status : Int
+    { id : RequestId
+    , status : Status
     , body : String
     }
 
 
+{-| Stateless worker - all request handling is synchronous
+-}
 type alias Model =
     ()
 
@@ -45,40 +68,31 @@ type Msg
 
 
 
--- ROUTES
+-- ROUTING
 
 
-type alias Handler =
-    Request -> Response
+route : Request -> Response
+route req =
+    case ( req.method, req.path ) of
+        ( Get, "/" ) ->
+            respond Ok "Welcome to Elm Backend!" req
+
+        ( Get, "/hello" ) ->
+            respond Ok "Hello from Elm!" req
+
+        ( Post, "/echo" ) ->
+            respond Ok req.body req
+
+        _ ->
+            respond NotFound "Not Found" req
 
 
-routes : Dict ( String, String ) Handler
-routes =
-    Dict.fromList
-        [ ( ( "GET", "/" ), homeHandler )
-        , ( ( "GET", "/hello" ), helloHandler )
-        , ( ( "POST", "/echo" ), echoHandler )
-        ]
-
-
-homeHandler : Handler
-homeHandler req =
-    { id = req.id, status = 200, body = "Welcome to Elm Backend!" }
-
-
-helloHandler : Handler
-helloHandler req =
-    { id = req.id, status = 200, body = "Hello from Elm!" }
-
-
-echoHandler : Handler
-echoHandler req =
-    { id = req.id, status = 200, body = req.body }
-
-
-notFound : Handler
-notFound req =
-    { id = req.id, status = 404, body = "Not Found" }
+respond : Status -> String -> Request -> Response
+respond status body req =
+    { id = req.id
+    , status = status
+    , body = body
+    }
 
 
 
@@ -88,55 +102,145 @@ notFound req =
 main : Program () Model Msg
 main =
     Platform.worker
-        { init = \_ -> ( (), Cmd.none )
+        { init = init
         , update = update
-        , subscriptions = \_ -> request GotRequest
+        , subscriptions = subscriptions
         }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( (), Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    request GotRequest
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         GotRequest value ->
-            case Decode.decodeValue requestDecoder value of
-                Ok req ->
-                    let
-                        handler =
-                            Dict.get ( req.method, req.path ) routes
-                                |> Maybe.withDefault notFound
-                    in
-                    ( model, response (encodeResponse (handler req)) )
+            let
+                result =
+                    decodeRequest value
+                        |> Result.map route
+                        |> Result.mapError (errorResponse value)
+            in
+            case result of
+                Result.Ok res ->
+                    ( model, response (encodeResponse res) )
 
-                Err err ->
-                    ( model
-                    , response
-                        (encodeResponse
-                            { id = "error"
-                            , status = 500
-                            , body = Decode.errorToString err
-                            }
-                        )
-                    )
+                Result.Err res ->
+                    ( model, response (encodeResponse res) )
+
+
+{-| Extracts request ID from raw JSON for error correlation
+-}
+errorResponse : Decode.Value -> Decode.Error -> Response
+errorResponse value err =
+    let
+        maybeId =
+            Decode.decodeValue (Decode.field "id" Decode.string) value
+                |> Result.toMaybe
+                |> Maybe.map RequestId
+                |> Maybe.withDefault (RequestId "unknown")
+    in
+    { id = maybeId
+    , status = BadRequest
+    , body = Decode.errorToString err
+    }
 
 
 
 -- JSON ENCODERS/DECODERS
 
 
+decodeRequest : Decode.Value -> Result Decode.Error Request
+decodeRequest =
+    Decode.decodeValue requestDecoder
+
+
 requestDecoder : Decoder Request
 requestDecoder =
-    Decode.map5 Request
-        (Decode.field "id" Decode.string)
-        (Decode.field "method" Decode.string)
+    Decode.map4 Request
+        (Decode.field "id" requestIdDecoder)
+        (Decode.field "method" methodDecoder)
         (Decode.field "path" Decode.string)
         (Decode.field "body" Decode.string)
-        (Decode.field "headers" (Decode.dict Decode.string))
+
+
+requestIdDecoder : Decoder RequestId
+requestIdDecoder =
+    Decode.map RequestId Decode.string
+
+
+methodDecoder : Decoder Method
+methodDecoder =
+    Decode.string
+        |> Decode.andThen
+            (\str ->
+                case str of
+                    "GET" ->
+                        Decode.succeed Get
+
+                    "HEAD" ->
+                        Decode.succeed Head
+
+                    "POST" ->
+                        Decode.succeed Post
+
+                    "PUT" ->
+                        Decode.succeed Put
+
+                    "DELETE" ->
+                        Decode.succeed Delete
+
+                    "CONNECT" ->
+                        Decode.succeed Connect
+
+                    "OPTIONS" ->
+                        Decode.succeed Options
+
+                    "TRACE" ->
+                        Decode.succeed Trace
+
+                    "PATCH" ->
+                        Decode.succeed Patch
+
+                    _ ->
+                        Decode.fail ("Unknown method: " ++ str)
+            )
 
 
 encodeResponse : Response -> Encode.Value
 encodeResponse resp =
     Encode.object
-        [ ( "id", Encode.string resp.id )
-        , ( "status", Encode.int resp.status )
+        [ ( "id", encodeRequestId resp.id )
+        , ( "status", encodeStatus resp.status )
         , ( "body", Encode.string resp.body )
         ]
+
+
+encodeRequestId : RequestId -> Encode.Value
+encodeRequestId (RequestId id) =
+    Encode.string id
+
+
+encodeStatus : Status -> Encode.Value
+encodeStatus status =
+    Encode.int
+        (case status of
+            Ok ->
+                200
+
+            BadRequest ->
+                400
+
+            NotFound ->
+                404
+
+            InternalError ->
+                500
+        )
